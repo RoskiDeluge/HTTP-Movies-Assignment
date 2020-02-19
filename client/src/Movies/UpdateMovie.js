46ff561e6ce5470d3e8bde72723f2b8ec2e84010
RoskiDeluge <roberto.delgado@gmail.com>
2020-02-18 18:59:40 -1000

    Working on useEffect hook for UpdateMovie

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,9 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
+const initialMovie = {
+    id: "",
+    title: "",
+    director: "",
+    metascore: "",
+    stars: []
+}
 
-const UpdateMovie = () => {
+const UpdateMovie = props => {
+    const [ movie, setMovie ] = useState(initialMovie);
+
+    useEffect(() => {
+        const selectedMovie = props.list.find(item => {
+            return `${item.id}` === props.match.params.id;
+        });
+        console.log(selectedMovie);
+    }, [])
+
     return (
-        <div>I'm an update form</div>
+        <div>
+            <h2>Update Movie</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="title"
+                    onChange={handleChange}
+                    placeholder="Movie Title"
+                    value={movie.name}
+                />
+                <input
+                    type="text"
+                    name="director"
+                    onChange={handleChange}
+                    placeholder="Movie Director"
+                    value={movie.director}
+                />
+                <input
+                    type="number"
+                    name="metascore"
+                    onChange={handleChange}
+                    placeholder="Movie Metascore"
+                    value={movie.metascore}
+                />
+                <input
+                    type="string"
+                    name="stars"
+                    onChange={handleChange}
+                    placeholder="Movie Stars"
+                    value={movie.stars}
+                />
+            </form>
+        </div>
     )
 }
 
